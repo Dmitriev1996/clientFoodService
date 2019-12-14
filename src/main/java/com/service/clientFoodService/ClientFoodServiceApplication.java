a0770910cb30a5d39967ad1d21e8e4af0a01c401
dmitriev <aleksandr_dmitriev_96@bk.ru>
2019-12-14 13:55:11 +0300

    create project

---
+++ src/main/java/com/service/clientFoodService/ClientFoodServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.clientFoodService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientFoodServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientFoodServiceApplication.class, args);
+	}
+
+}
